e333acdbe3978c8da125507d3162c6651b58640f
Vitalii <petruk.v.m2@gmail.com>
2021-02-15 18:53:14 +0200

    Wrong

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import styled from 'styled-components';
 
-const App = () => (
-  <h1>My React and TypeScript App!</h1>
-);
+Sentry.init({
+  dsn: 'https://5713e176114d48018bc11a3a500b2654@o494472.ingest.sentry.io/5565601',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+const App = () => {
+  const fail = () => {
+    const styledC: any = styled;
+    console.log(styledC);
+    document.body.style.background = "red";
+  };
+
+  return (
+  <h1 onClick={fail}>My React and TypeScript App!</h1>
+)};
 
 ReactDOM.render(
-  <App />,
+  <Sentry.ErrorBoundary fallback="An error has occurred">
+    <App />
+  </Sentry.ErrorBoundary>,
   document.getElementById("root")
 );
