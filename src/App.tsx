43304f59f4f3ef23e5d4f1703272a9570639ba83
Vitalii <petruk.v.m2@gmail.com>
2021-02-18 17:52:27 +0200

    react-router-dom update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter, Route,
+} from 'react-router-dom';
 import Home from './Home';
 
 const App: React.FC = () => (
-  <Home />
+  <BrowserRouter>
+    <Route path="/" component={Home} exact />
+  </BrowserRouter>
 );
 
 export default App;
