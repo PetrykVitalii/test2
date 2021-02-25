02fe45afb37a08249b47cf06ad216222e9aeca65
Vitalii <petruk.v.m2@gmail.com>
2021-02-25 17:10:08 +0200

    suspense

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,25 @@ import React, { Suspense } from 'react';
 import {
   BrowserRouter, Switch, Route,
 } from 'react-router-dom';
+import Welcome from './Welcome';
 // import E from './E';
 // import W from './W';
 // import Welcome from './Welcome';
-const E = React.lazy(() => import('./E'));
-const W = React.lazy(() => import('./W'));
-const Welcome = React.lazy(() => import('./Welcome'));
+// const E = React.lazy(() => import('./E'));
+// const W = React.lazy(() => import('./W'));
+// const Welcome = React.lazy(() => import('./Welcome'));
 
 interface Props {
 }
 
 const App: React.FC<Props> = () => (
   <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Welcome />
-      {/* <Switch>
+    <Welcome />
+    {/* <Switch>
         <Route path="/" component={Welcome} exact />
         <Route path="/w" component={W} exact />
         <Route path="/e" component={E} exact />
       </Switch> */}
-    </Suspense>
   </BrowserRouter>
 );
 
