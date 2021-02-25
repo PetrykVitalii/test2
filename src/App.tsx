8525cbba6a46f38be1e2c615f8af80cd4b8ca27a
Vitalii <petruk.v.m2@gmail.com>
2021-02-25 17:04:57 +0200

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import React, { Suspense } from 'react'
+import React, { Suspense } from 'react';
 import {
   BrowserRouter, Switch, Route,
 } from 'react-router-dom';
@@ -14,20 +14,17 @@ const Welcome = React.lazy(() => import('./Welcome'));
 interface Props {
 }
 
-const App: React.FC<Props> = () => {
-    return (
-      <BrowserRouter>
-       <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/" component={Welcome} exact />
-            <Route path="/w" component={W} exact />
-            <Route path="/e" component={E} exact />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    )
-};
-  
-
+const App: React.FC<Props> = () => (
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Welcome />
+      {/* <Switch>
+        <Route path="/" component={Welcome} exact />
+        <Route path="/w" component={W} exact />
+        <Route path="/e" component={E} exact />
+      </Switch> */}
+    </Suspense>
+  </BrowserRouter>
+);
 
 export default App;
