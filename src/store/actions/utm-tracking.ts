ee3d418c44253df6116e90060a484681c8461bf1
Vitalii <petruk.v.m2@gmail.com>
2021-03-01 13:03:21 +0200

    from entries

--- src/store/actions/utm-tracking.ts
+++ src/store/actions/utm-tracking.ts
@@ -1,4 +1,6 @@
 /* eslint-disable no-param-reassign */
+import queryString from 'query-string';
+import LocalStorage from '@/utils/local-storage';
 import { AsyncAction } from './common';
 
 export interface IUtm {
@@ -11,40 +13,40 @@ export interface IUtm {
   utm_term?: string;
 }
 
-// const allUtms: IUtm = {
-//   utm_source: 'OK',
-//   utm_medium: 'OK',
-//   utm_campaign: 'OK',
-//   gclid: 'OK',
-//   fbclid: 'OK',
-//   utm_content: 'OK',
-//   utm_term: 'OK',
-// };
+const allUtms: IUtm = {
+  utm_source: 'OK',
+  utm_medium: 'OK',
+  utm_campaign: 'OK',
+  gclid: 'OK',
+  fbclid: 'OK',
+  utm_content: 'OK',
+  utm_term: 'OK',
+};
 
-export const trackUtm = (): AsyncAction => async () => {
-  // const { router } = getState();
-  // const { search } = router.location;
+export const trackUtm = (): AsyncAction => async (_, getState) => {
+  const { router } = getState();
+  const { search } = router.location;
 
-  // const searchUtms = queryString.parse(search) as IUtm;
+  const searchUtms = queryString.parse(search) as IUtm;
 
-  // const localStorageUtms = LocalStorage.getUTM() || {};
+  const localStorageUtms = LocalStorage.getUTM() || {};
 
-  // const entriesUtms = Object.entries(searchUtms) as [keyof IUtm, string][];
+  const entriesUtms = Object.entries(searchUtms) as [keyof IUtm, string][];
 
-  // const utmsToSave = entriesUtms.reduce(
-  //   (utms: IUtm, [key, value]) => {
-  //     if (allUtms[key]) {
-  //       const isEmpty = !localStorageUtms[key];
-  //       if (isEmpty) {
-  //         utms[key] = value;
-  //       } else {
-  //         utms[key] = localStorageUtms[key];
-  //       }
-  //     }
-  //     return utms;
-  //   },
-  //   {},
-  // );
+  const utmsToSave = entriesUtms.reduce(
+    (utms: IUtm, [key, value]) => {
+      if (allUtms[key]) {
+        const isEmpty = !localStorageUtms[key];
+        if (isEmpty) {
+          utms[key] = value;
+        } else {
+          utms[key] = localStorageUtms[key];
+        }
+      }
+      return utms;
+    },
+    {},
+  );
 
-  // LocalStorage.setUTM(utmsToSave);
+  LocalStorage.setUTM(utmsToSave);
 };
